5eae5167c902385e5bc3d19849484d3d480dc504
johnmiicheal <elijah.johnmicheal@stu.cu.edu.ng>
2021-03-16 12:55:31 +0100

    update frames

--- main.py
+++ main.py
@@ -1,6 +1,21 @@
 import pygame
 from defs import *
 
+
+def update_label(data, title, font, x, y, gameDisplay):
+    label = font.render('{} {}'.format(title, data), 1, DATA_FONT_COLOR)
+    gameDisplay.blit(label, (x,y))
+    return y
+
+def update_data_labels(gameDisplay, dt, game_time, font):
+    y_pos = 10
+    gap = 20
+    x_pos = 10
+    y_pos = update_label(round(1000/dt,2), 'FPS', font, x_pos, y_pos + gap, gameDisplay)
+    y_pos = update_label(round(game_time/1000,2), 'Game Time', font, x_pos, y_pos + gap, gameDisplay)
+
+
+
 def run_game():
     pygame.init()
     gameDisplay = pygame.display.set_mode((DISPLAY_W, DISPLAY_H))
@@ -8,6 +23,7 @@ def run_game():
 
     running = True
     bgImg = pygame.image.load(BG_FILENAME)
+    label_font = pygame.font.SysFont("monospace", DATA_FONT_SIZE)
 
     clock = pygame.time.Clock()
     dt = 0
@@ -27,6 +43,7 @@ def run_game():
             elif event.type == pygame.KEYDOWN:
                 running = False
 
+        update_data_labels(gameDisplay, dt, game_time, label_font)
         pygame.display.update()
 
 
